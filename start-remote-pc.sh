4c5f1586286e1e3221285e58ab4b243a400e3a72
Mangmasta <mangmasta@gmail.com>
2021-06-23 22:04:05 -0700

    Made the docker script work.  very WIP though.

--- start-remote-pc.sh
+++ start-remote-pc.sh
@@ -3,7 +3,7 @@ RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-JOYDEV=/dev/null
+#JOYDEV=/dev/null
 for ((i=0; i <=5; i++)); do
 #printf "testing /dev/input/js$i\n"
     if [ -e "/dev/input/js$i" ]; then
@@ -12,12 +12,22 @@ for ((i=0; i <=5; i++)); do
         break
     fi
 done
-if command -v udevadm &> /dev/null; then
-    printf "Dev is %s\n" `udevadm info --attribute-walk --name $JOYDEV|grep -Po '(?<=name}==")(.*)(?<=")'`
-    #'(?:.*ATTRS\{name\}==\")(.*)(?:\")'`
-fi
+
+#eh forget about this for now i dont want to spend time making it work
+#if command -v udevadm &> /dev/null; then
+#    printf "Dev is %s\n" `udevadm info --attribute-walk --name $JOYDEV|grep -Po '(?<=name}==")(.*)(?<=")'`
+#    #'(?:.*ATTRS\{name\}==\")(.*)(?:\")'`
+#fi
 #printf
+ROSHOST=`ip -4 addr show enp5s0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`
+#if [[ -z "${JOYDEV}" ]]; then
+#    DOCPARAMS=-e 
 
-printf "${YELLOW}Warning: Controller not detected.  The joy node will be non-operational.${NC}\n"
+#else
+#fi
+DOCPARAMS="-e ROS_HOSTNAME=localhost -v $PWD:/ros -w /ros"
 
-#docker run --rm -it $(docker build -q docker)
+
+printf "${YELLOW}Warning: Controller not detected.  The joy node will be non-operational. [WIP]${NC}\n"
+
+docker run $DOCPARAMS --rm -it $(docker build -q docker) /bin/bash
