f41968ea6e43c53a283ec5627e48658fb7f7732a
Fenteale <mangmasta@gmail.com>
2021-07-06 17:31:52 -0700

    Fixed returning the first ip address found on the host to HOSTIP

--- start-remote-pc.sh
+++ start-remote-pc.sh
@@ -15,8 +15,9 @@ for ((i=0; i <=5; i++)); do
 done
 
 #omega long command to get list of local IP addresses.  lots of regex
-HOSTIP=`ip addr show|grep -Po 'inet.*(?<!lo|docker\d)$'|grep -Po '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?=/)'`
-printf "Host ip: %s\n", ${HOSTIP[0]}
+HOSTIP=`ip addr show|grep -Po 'inet.*(?<!lo|docker\d)$'|grep -m 1 -Po '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?=/)'`
+# the "-m 1" arg for the last grep makes it return only the first match
+printf "Host ip: %s\n", ${HOSTIP}
 
 if [[ -z "${JOYDEV}" ]]; then
     printf "${YELLOW}Warning: Controller not detected.  The joy node will be non-operational.${NC}\n"
@@ -25,7 +26,7 @@ else
 fi
 
 
-DOCPARAMS="--network host -e ROS_MASTER_URI=http://${HOSTIP[0]}:11311 -e ROS_HOSTNAME=${HOSTIP[0]} -v $PWD:/ros -w /ros"
+DOCPARAMS="--network host -e ROS_MASTER_URI=http://${HOSTIP}:11311 -e ROS_HOSTNAME=${HOSTIP} -v $PWD:/ros -w /ros"
 
 docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) docker -t rmc:ros
 docker run $DOCPARAMS $JOYPARAM --rm -it rmc:ros /bin/bash
